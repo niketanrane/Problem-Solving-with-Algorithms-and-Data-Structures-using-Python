56fdd5c5f3f57a658d3e10e57fd59067aa29d5ab
Niketan Santosh Rane <40956529+niketanrane@users.noreply.github.com>
2018-08-11 17:42:25 +0530

    Add files via upload
    Dequeue implementation in Python

---
+++ 3_Basic_Data_Structures/Dequeue_3_17.py
@@ -0,0 +1,27 @@
+__author__ = "niketanrane"
+
+class Dequeue:
+    '''
+    Dequeue implementation in Python
+    '''
+    def __init__(self):
+        self.items = []
+
+    def size(self):
+        return len(self.items)
+
+    def isEmpty(self):
+        return self.items == []
+
+    def addFront(self, value):
+        self.items.append(value)
+
+    def addRear(self, value):
+        self.itmes.insert(0, value)
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
