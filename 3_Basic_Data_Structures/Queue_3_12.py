5d390b604fecce03d8537ccd4ab1710b52b3e043
Niketan Santosh Rane <40956529+niketanrane@users.noreply.github.com>
2018-08-11 15:06:45 +0530

    Add files via upload
    A Queue implemented in Python 3.6

---
+++ 3_Basic_Data_Structures/Queue_3_12.py
@@ -0,0 +1,23 @@
+__author__ = "niketanrane"
+
+class Queue:
+    '''
+    A queue class implementation in Python
+    '''
+    def __init__(self):
+        self.items = []
+
+    def size(self):
+        return len(self.items)
+
+    def enqueue(self, value):
+        self.items.insert(0, value)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def isEmpty(self):
+        return len(self.items) == 0
+
+
+
