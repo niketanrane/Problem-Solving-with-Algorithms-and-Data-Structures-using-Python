eb00ee3840a9bc212a218e6ec5b0dda6f4b8645d
Niketan Santosh Rane <40956529+niketanrane@users.noreply.github.com>
2018-08-13 16:13:12 +0530

    Add files via upload

--- 5_Sorting_And_Searching/Shell_Sort_5_10.py
+++ 5_Sorting_And_Searching/Shell_Sort_5_10.py
@@ -11,7 +11,7 @@ def gapInsertionSort(aList, start, gap):
 
     print("After this iteration list is", aList)
 
-    
+
 def shellSort(aList):
     sublistcount = len(aList) // 2
     while sublistcount > 0:
@@ -26,3 +26,9 @@ def shellSort(aList):
 alist = [54,26,93,17,77,31,44,55,20]
 shellSort(alist)
 print(alist)
+
+'''
+Note: We can say that big O-h tends to fall somewhere between O(n) and O(n2). For the increments 
+shown in Listing 5, the performance is O(n^2). By changing the increment, for example using 2k−1 (1, 3, 7, 15, 31, and so on), 
+a shell sort can perform at O(n^(1.5)).
+'''
